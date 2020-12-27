bb698e8ebcdf35806f3c306d11fcef3e69fdec7b
Ebru Burhan <ebruburhan1@gmail.com>
2020-12-27 20:19:05 +0300

    User controllerda User listeleme ve User ekleme viewları yapıldı post edilmedi user

---
+++ Blog/Controllers/UserController.cs
@@ -0,0 +1,36 @@
+﻿using Blog.Models.EntityFramework;
+using Blog.Models.ViewsModels;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Blog.Controllers
+{
+    public class UserController : Controller
+    {
+        BlogContext _db = new BlogContext();
+
+        [HttpGet]
+        public ActionResult Index()
+        {
+            ListUserVm model = new ListUserVm()
+            {
+                Users = _db.Users.ToList()
+            };
+            return View(model);
+        }
+
+        [HttpGet]
+        public ActionResult AddUser()
+        {
+            UserManipulationVm model = new UserManipulationVm()
+            {
+                User = new User()
+            };
+
+            return View(model);
+        }
+    }
+}
---
+++ Blog/Models/ViewsModels/ListUserVm.cs
@@ -0,0 +1,13 @@
+﻿using Blog.Models.EntityFramework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Blog.Models.ViewsModels
+{
+    public class ListUserVm
+    {
+        public List<User> Users { get; set; }
+    }
+}
